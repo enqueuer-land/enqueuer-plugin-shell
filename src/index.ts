1d1539e882eb576ea556e8d3b364bef7853a8d43
Guilherme Moraes <guilherme.moraes@ifood.com.br>
2020-03-16 18:48:31 -0300

    Fix #16 & remove spawn feature

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 import * as exec from './exec-publisher';
-import * as spawn from './spawn-publisher';
 import {MainInstance} from 'enqueuer';
 
 export function entryPoint(mainInstance: MainInstance): void {
     exec.entryPoint(mainInstance);
-    spawn.entryPoint(mainInstance);
 }
